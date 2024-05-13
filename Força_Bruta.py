e8784225ff00ecc933233394f14a1410524d8514
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-05-13 10:34:22 -0300

    13/05

--- Força_Bruta.py
+++ Força_Bruta.py
@@ -13,18 +13,18 @@ with open('rota.txt', 'r') as entrada:
 
     # localizar os pontos e as coordenadas
     for l in range(len(matriz)): # n de linhas
-        for c in range(len(matriz[l])): # n de elementos dentro da linha
+        for c in range(len(matriz[0])): # n de elementos dentro da linha
             if matriz[l][c] != '0':
                 pontos[matriz[l][c]] = [l, c] # chave = ponto ... valor = coordenada
 
-print(pontos)
+#print(pontos)
 
 # calcular as distâncias entre os pontos
 def distancias(coordenada1, cooordenada2):
     return abs((coordenada1[0]) - (cooordenada2[0])) + abs( (coordenada1[1]) - (cooordenada2[1]))
 
 # permutando as rotas
-recebidos = permutations(pontos)
+permutações = permutations(pontos)
 
 # for j in recebidos:
 #     print(j) # todas as combinações
@@ -32,22 +32,22 @@ recebidos = permutations(pontos)
 # calcular todas as distâncias e encontrar a menor delas
 distancia = []
 rotas = {maxsize: ('R', 'D')}
-for i in list(recebidos):
+for i in list(permutações):
     if i[0] == "R":
         for coordenada in range(len(i) - 1):
             distancia.append(distancias(pontos[i[coordenada]], pontos[i[coordenada+1]]))
-            # voltando para o início = "R"
-            distancia.append(distancias(pontos[i[coordenada+1]], pontos["R"]))
+        # voltando para o início = "R"
+        distancia.append(distancias(pontos[i[coordenada+1]], pontos["R"]))
 
             # seleção da menor rota
-            menor_rota = list(rotas.keys())
-            if sum(distancia) < int(menor_rota[0]):
-                rotas.clear()
-                rotas[sum(distancia)] = i
+        menor_rota = list(rotas.keys())
+        if sum(distancia) < int(menor_rota[0]):
+            rotas.clear()
+            rotas[sum(distancia)] = i
 
-            distancia = []
+        distancia = []
 
-            print(rotas)
+            #print(rotas)
 
 # tirar o 'r' e formatar em uma única string visual
 menor_rota_visual = list(rotas[int(menor_rota[0])])
