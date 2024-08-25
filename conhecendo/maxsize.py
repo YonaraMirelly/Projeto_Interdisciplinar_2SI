872f3daa0179b65e297b82797b542568bdf077d1
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-08-25 20:07:03 -0300

    commit

--- conhecendo/maxsize.py
+++
@@ -1,7 +0,0 @@
-from sys import maxsize
-# representa o maior número possível ou o menor número possível. Exemplo -> em bits de 64, o maxsize representa 2^63 = numero grandíssimo
-# tbm pode ser representado por float('inf')
-melhor = maxsize
-print(melhor)
-menor = - maxsize 
-print(menor)
